e4da2b950e3d34649a3a16e8a51be72f7719dd2c
SUNNYsyy2005 <141478683+SUNNYsyy2005@users.noreply.github.com>
2023-10-16 16:16:15 +0800

    Add files via upload

---
+++ exercises/macros/macros3.rs
@@ -0,0 +1,19 @@
+// macros3.rs
+//
+// Make me compile, without taking the macro out of the module!
+//
+// Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
+// hint.
+
+#[macro_use]
+mod macrosa {
+    macro_rules! my_macroa {
+        () => {
+            println!("Check out my macro!");
+        };
+    }
+}
+
+fn main() {
+    my_macroa!();
+}
