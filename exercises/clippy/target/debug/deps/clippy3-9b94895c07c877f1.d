60da6b8913fe91a49517940907f68f8f467a7ecb
SUNNYsyy2005 <141478683+SUNNYsyy2005@users.noreply.github.com>
2023-10-16 16:14:38 +0800

    Add files via upload

---
+++ exercises/clippy/target/debug/deps/clippy3-9b94895c07c877f1.d
@@ -0,0 +1,9 @@
+D:\Users\s7073\Source\Repos\LearningOS\rust-rustlings-2023-autumn-SUNNYsyy2005\exercises\clippy\target\debug\deps\clippy3-9b94895c07c877f1.rmeta: clippy3.rs Cargo.toml
+
+D:\Users\s7073\Source\Repos\LearningOS\rust-rustlings-2023-autumn-SUNNYsyy2005\exercises\clippy\target\debug\deps\clippy3-9b94895c07c877f1.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
