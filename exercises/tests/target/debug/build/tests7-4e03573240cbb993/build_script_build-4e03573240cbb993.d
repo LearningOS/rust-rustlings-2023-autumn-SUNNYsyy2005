c5bd9b37ca8aae0e6170d5cc6dd88f68bc12f5da
SUNNYsyy2005 <141478683+SUNNYsyy2005@users.noreply.github.com>
2023-10-16 16:21:47 +0800

    Add files via upload

---
+++ exercises/tests/target/debug/build/tests7-4e03573240cbb993/build_script_build-4e03573240cbb993.d
@@ -0,0 +1,5 @@
+D:\Users\s7073\Source\Repos\LearningOS\rust-rustlings-2023-autumn-SUNNYsyy2005\exercises\tests\target\debug\build\tests7-4e03573240cbb993\build_script_build-4e03573240cbb993.exe: build.rs
+
+D:\Users\s7073\Source\Repos\LearningOS\rust-rustlings-2023-autumn-SUNNYsyy2005\exercises\tests\target\debug\build\tests7-4e03573240cbb993\build_script_build-4e03573240cbb993.d: build.rs
+
+build.rs:
---
+++ exercises/tests/target/debug/build/tests8-75de1140bd3a8bb4/build_script_build-75de1140bd3a8bb4.d
@@ -0,0 +1,5 @@
+D:\Users\s7073\Source\Repos\LearningOS\rust-rustlings-2023-autumn-SUNNYsyy2005\exercises\tests\target\debug\build\tests8-75de1140bd3a8bb4\build_script_build-75de1140bd3a8bb4.exe: build.rs
+
+D:\Users\s7073\Source\Repos\LearningOS\rust-rustlings-2023-autumn-SUNNYsyy2005\exercises\tests\target\debug\build\tests8-75de1140bd3a8bb4\build_script_build-75de1140bd3a8bb4.d: build.rs
+
+build.rs:
