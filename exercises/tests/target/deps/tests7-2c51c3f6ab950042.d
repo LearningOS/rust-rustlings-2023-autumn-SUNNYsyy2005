c899bc98dd8a5551c8d01a41181a3dea2ca72a55
SUNNYsyy2005 <141478683+SUNNYsyy2005@users.noreply.github.com>
2023-10-16 16:24:31 +0800

    Add files via upload

---
+++ exercises/tests/target/deps/tests7-2c51c3f6ab950042.d
@@ -0,0 +1,5 @@
+D:\Users\s7073\Source\Repos\LearningOS\rust-rustlings-2023-autumn-SUNNYsyy2005\exercises\tests\target\debug\deps\tests7-2c51c3f6ab950042.exe: tests7.rs
+
+D:\Users\s7073\Source\Repos\LearningOS\rust-rustlings-2023-autumn-SUNNYsyy2005\exercises\tests\target\debug\deps\tests7-2c51c3f6ab950042.d: tests7.rs
+
+tests7.rs:
---
+++ exercises/tests/target/deps/tests8-c5b80543de9456ce.d
@@ -0,0 +1,5 @@
+D:\Users\s7073\Source\Repos\LearningOS\rust-rustlings-2023-autumn-SUNNYsyy2005\exercises\tests\target\debug\deps\tests8-c5b80543de9456ce.exe: tests8.rs
+
+D:\Users\s7073\Source\Repos\LearningOS\rust-rustlings-2023-autumn-SUNNYsyy2005\exercises\tests\target\debug\deps\tests8-c5b80543de9456ce.d: tests8.rs
+
+tests8.rs:
